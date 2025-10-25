d6c5c75aa5e41ddfee78e7574b8e53c60a1e2bc3
Pratima <prateemadhakal555@gmail.com>
2025-10-25 22:05:45 +0545

    swag_lag login page automation

--- firsttest.py
+++ firsttest.py
@@ -1,5 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
+#open brower
 driver =webdriver.Chrome()
-driver.get("https://www.google.com")
-driver.quit()
+
+#go to url
+driver.get("https://www.saucedemo.com/")
+
+time.sleep(10)
+
+#element locate then value send
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+
+driver.find_element(By.XPATH,"//input[@id ='login-button']").click()
+time.sleep(4)
+print("login sucessful")
+
