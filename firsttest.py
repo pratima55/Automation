10ca03bfec593311c3564ef5b6f2423723d605dd
Pratima <prateemadhakal555@gmail.com>
2025-10-28 13:59:58 +0545

    something added

--- firsttest.py
+++ firsttest.py
@@ -8,13 +8,19 @@ driver =webdriver.Chrome()
 #go to url
 driver.get("https://www.saucedemo.com/")
 
-time.sleep(10)
+time.sleep(4)
 
 #element locate then value send
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "user-name").send_keys("standard_user") 
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 
 driver.find_element(By.XPATH,"//input[@id ='login-button']").click()
 time.sleep(4)
-print("login sucessful")
+
+
+
+assert driver.current_url == "https://www.saucedemo.com/cart.html", "url mismatch after login"
+assert driver.find_element(By.ID, "react-burger-menu-btn").is_displayed()
+print("test passed")
+driver.quit()
 
